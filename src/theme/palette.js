efd7fd242db24009be4a8c5b2fd2940f49a444fb
Kate Mills <helloworldloveksm@icloud.com>
2021-12-12 19:33:38 -0800

    cleanup

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -1,33 +1,3 @@
-/*const green = {
-  light: '#959b9b',
-  main: '#435849',
-  dark: '#37383a',
-  contrastText: '#fff',
-}
-const lightPink = {
-  light: '#f8f8f8',
-  main: '#fabfb9',
-  dark: '#959b9b',
-  contrastText: '#000',
-}
-const pink = {
-  light: '#fabfb9',
-  main: '#c68f89',
-  dark: '#94615c',
-  contrastText: '#000',
-}
-const grey = {
-  light: '#f8f8f8',
-  main: '#959b9b',
-  dark: '#676d6d',
-  contrastText: '#fff',
-}
-const glamour = {
-  light: '#ffffff',
-  main: '#fbf7ea',
-  dark: '#c8c4b8',
-  contrastText: '#0c0c0c',
-}*/
 const gold = {
   light: '#f0da8d',
   main: '#b99541',
@@ -38,18 +8,25 @@ const taupe = {
   light: '#fbf7ea',
   main: '#c8c4b8',
   dark: '#979488',
-  contrastText: '#0c0c0c',
+  contrastText: '#0f0f0f',
+}
+const buttons = {
+  light: '#c8c4b8',
+  main: '#979488',
+  dark: '#4e4c3f',
+  contrastText: '#fff',
 }
 
 export const light = {
   alternate: {
-    main: "#f9f9f7", //#ebebeb
+    main: "#f9f9f7",
     dark: "#ebebeb",
   },
   cardShadow: 'rgba(23, 70, 161, .11)',
   mode: 'light',
   primary: taupe,
   secondary: gold,
+  buttons,
   text: {
      primary: "#32332a",
     secondary: "#4F554D",
@@ -64,7 +41,6 @@ export const light = {
   },
 };
 
-
 export const dark = {
   alternate: {
     main: '#37383a',
@@ -75,23 +51,20 @@ export const dark = {
     black: '#000',
     white: '#fff',
   },
-
   mode: 'dark',
   primary: taupe,
   secondary: gold,
-
+  buttons,
   text: {
     primary: '#ffffff',
     secondary: '#e3e3e3',
   },
-
   divider: 'rgba(255, 255, 255, 0.12)',
   background: {
     paper: '#3a383d',
-    default: '#111214', //#040405
-    level1: '#020205', //#222B45
-    level2: '#111214', //#040405
+    default: '#111214',
+    level1: '#020205',
+    level2: '#111214',
     footer: "#18181f",
   },
 };
-
