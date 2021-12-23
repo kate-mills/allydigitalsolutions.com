0532bc02f2ef2d8cd5290491a37e9f73e1f59754
Kate Mills <helloworldloveksm@icloud.com>
2021-12-22 17:52:50 -0800

    google analytics update

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,21 +78,21 @@ module.exports = {
       options:{
         trackingId: "UA-215693750-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        optimizeId: "OPT-M5K5N3P",
         // Enables Google Optimize Experiment ID
-        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        experimentId: "nfa82CprS161s3tAv5DHEg",
         // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        variationId: "0",
         // Defers execution of google analytics script after page load
         defer: false,
         // Any additional optional fields
