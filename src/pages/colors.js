4e78c016c988307bcca6f768c6d881e8624cf182
Kate Mills <helloworldloveksm@icloud.com>
2021-12-31 15:27:46 -0800

    2 new pages use Fixed Layout

---
+++ src/pages/colors.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Colors from 'views/Colors';
+
+const ColorsPage = () => {
+  return <Colors />;
+};
+
+export default ColorsPage;
---
+++ src/pages/illustrations.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Illustrations from 'views/Illustrations';
+
+const IllustrationsPage = () => {
+  return <Illustrations />;
+};
+
+export default IllustrationsPage;
