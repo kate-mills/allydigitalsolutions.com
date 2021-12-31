68bbbbb67ebc62148b4acaeb8d230648cb0d467a
Kate Mills <helloworldloveksm@icloud.com>
2021-12-31 13:43:27 -0800

    cosmo theme

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -1,78 +1,80 @@
-const darkP = {
-  light: '#2196f3',
-  main: '#1976d2',
-  dark: '#0d47a1',
-  contrastText: '#ffffff'
+const cosmoYellow = {
+  light: "#ffffce",
+  main: "#e9cd9d",
+  dark: "#b69c6e",
+  contrastText: "#000000",
 }
-
-const darkS = {
-  light: '#FFEA41',
-  main: '#FFE102',
-  dark: '#DBBE01',
-  contrastText: '#000000'
+const cosmoGold = {
+  light: "#ddb38d",
+  main: "#aa835f",
+  dark: "#795635",
+  contrastText: "#000406",
 }
-
-const lightP = {
-  light: '#467de3',
-  main: '#377dff',
-  dark: '#2f6ad9',
-  contrastText: '#FFFFFF'
+const cosmoSilver = {
+  light: "#ede9e7",
+  main: "#bbb7b5",
+  dark: "#8b8785",
+  contrastText: "#000000",
 }
-const lightS = {
-  light: '#ffb74d',
-  main: '#f9b934',
-  dark: '#FF9800',
-  contrastText: '#000000'
+const cosmoBlue = {
+  light: "#5093cc",
+  main: "#06669b",
+  dark: "#003c6c",
+  contrastText: "#ffffff",
 }
-
-
+const cosmoOrange = {
+  light: "#ffaa70",
+  main: "#EE7943",
+  dark: "#b64a16",
+  contrastText: "#000000",
+}
 export const light = {
   alternate: {
     main: "#f9f9f7",
     dark: "#ebebeb",
   },
-  cardShadow: 'rgba(23, 70, 161, .11)',
-  mode: 'light',
-  primary: lightP,
-  secondary: lightS,
+  cardShadow: "rgba(23, 70, 161, .11)",
+  mode: "light",
+  primary: cosmoBlue,
+  secondary: cosmoSilver,
   text: {
-     primary: "#031624",
-    secondary: "#53595b",
+     primary: "#34170B",
+    secondary: "#603e32",
   },
-  divider: 'rgba(0, 0, 0, 0.12)',
+  divider: "rgba(0, 0, 0, 0.12)",
   background: {
-    paper: '#ffffff',
-    default: '#ffffff',
-    level2: '#f5f5f5',
-    level1: '#ffffff',
+    paper: "#ffffff",
+    default: "#ffffff",
+    level2: "#f5f5f5",
+    level1: "#ffffff",
     footer: "#1b1642",
   },
 };
 
 export const dark = {
   alternate: {
-    main: '#1a2138',
-    dark: '#151a30',
+    main: "#1a2138",
+    dark: "#151a30",
   },
-  cardShadow: 'rgba(0, 0, 0, .11)',
+  cardShadow: "rgba(0, 0, 0, .11)",
   common: {
-    black: '#000',
-    white: '#fff',
-    red:'#400A18',
+    black: "#000",
+    white: "#fff",
+    grey: "#bbb7b5",
   },
-  mode: 'dark',
-  primary: darkP,// greenMonoForDarkBg,
-  secondary:darkS,
+  mode: "dark",
+  primary: cosmoYellow,
+  secondary:cosmoGold,
   text: {
-    primary: '#fff',
-    secondary: '#bec7cb',
+    primary: "#fff",
+    secondary: "#bec7cb",
   },
-  divider: 'rgba(255, 255, 255, 0.12)',
+  divider: "rgba(255, 255, 255, 0.12)",
   background: {
-    default: '#222B45', //#28292B
-    paper: '#222B45', //#262223
-    level1: '#2D3748',
-    level2: '#333',
+    default: "#222B45",
+    paper: "#222B45",
+    level1: "#2D3748",
+    level2: "#333",
     footer: "#2D2428",
   },
 };
